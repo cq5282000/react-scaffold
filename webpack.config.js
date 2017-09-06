38d3c18fbdbbfaa467cdfb0f145af771e7a04be9
chenqu <chenqu@chenqudeMacBook-Pro-2.local>
2017-09-06 09:26:08 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -41,12 +41,12 @@ const webpackConfig = {
     },
     plugins: [
         // new CleanWebpackPlugin(['dist']), // 清理dist文件
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify('production'),
-            },
-        }),
-        new webpack.optimize.UglifyJsPlugin(),
+        // new webpack.DefinePlugin({
+        //     'process.env': {
+        //         NODE_ENV: JSON.stringify('production'),
+        //     },
+        // }),
+        // new webpack.optimize.UglifyJsPlugin(),
         // new webpack.optimize.CommonsChunkPlugin({
         //     name: 'common',
         // }),
