cb93f2a1ac98fc284ef2509ca554214b3b263b8b
chenqu <chenqu@chenqudeMacBook-Pro-2.local>
2017-09-12 23:17:53 +0800

    add

---
+++ src/libs/createRender.js
@@ -0,0 +1,35 @@
+/**
+ * Created by chenqu on 2017/9/12.
+ */
+import React from 'react';
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import reducer from '../reducers';
+
+const logger = createLogger();
+const store = createStore(combineReducers(reducer), compose(
+    applyMiddleware(logger),
+    window.devToolsExtension ? window.devToolsExtension() : (f) => f,
+));
+
+if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('../reducers', () => {
+        const nextReducer = require('../reducers').default;
+        store.replaceReducer(combineReducers(nextReducer));
+    });
+}
+
+export default (Entry) => {
+    return render(
+        <AppContainer>
+            <Provider store={store}>
+                <Entry/>
+            </Provider>
+        </AppContainer>,
+        document.getElementById('app'),
+    );
+};
